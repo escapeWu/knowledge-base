80f4fe1d2017eb119f2342c554587fda404fce30
shancw <shancw1996@gmail.com>
2024-06-03 22:44:10 +0800

    chore: add npm cache folder

--- dg-deploy.sh
+++ dg-deploy.sh
@@ -1,7 +1,5 @@
-git pull origin main && \
- docker stop digital-garden && \
- docker rm -f digital-garden && \
- docker rmi digital-garden && \
-npm run build && \
-docker build -t digital-garden . &&\
-docker run -d -p 7777:7777 --name digital-garden digital-garden
+git pull origin main 
+npm run build
+cp -r npm build/npm
+docker compose down 
+docker compose up --build -d
